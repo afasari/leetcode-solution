ccca69c3e3a3676ea0839a3d480ec17a63ece7fe
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2023-10-16 07:25:59 +0700

    Time: 1 ms (78.65%), Space: 2 MB (48.70%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.go
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	result := head.Next
+	head.Next = swapPairs(head.Next.Next)
+	result.Next = head
+	return result
+}
