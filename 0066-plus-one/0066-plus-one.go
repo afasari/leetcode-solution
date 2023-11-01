e451d33acf14a60381f1e83c8644afb597dd4a9e
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2023-11-01 08:36:43 +0700

    Time: 1 ms (76.38%), Space: 2.1 MB (52.68%) - LeetHub

--- 0066-plus-one/0066-plus-one.go
+++ 0066-plus-one/0066-plus-one.go
@@ -5,19 +5,20 @@ func plusOne(digits []int) []int {
         digits[ld-1] +=1
         return digits
     } 
-        for i:= ld-1; i >= 0; i--{
-            inc := digits[i] 
-            
-            if inc != 9{
-                digits[i] += 1
-                break
-            }
-            
-            digits[i] = 0
-            if i == 0{
-                digits = append([]int{1}, digits...)
-            }
+    
+    for i:= ld-1; i >= 0; i--{
+        inc := digits[i] 
+
+        if inc != 9{
+            digits[i] += 1
+            break
         }
+
+        digits[i] = 0
+        if i == 0{
+            digits = append([]int{1}, digits...)
+        }
+    }
     
     return digits
     
