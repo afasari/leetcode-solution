8b444520e6c9defb74319da7a406d25d15a0b220
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2023-11-14 07:35:09 +0700

    Time: 0 ms (100.00%), Space: 2 MB (92.23%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let mut memo = HashMap::new();
+        pub fn climber(n: i32, memo: &mut HashMap<i32, i32>) -> i32 {
+            match n {
+                1 => { return 1; },
+                2 => { return 2; },
+                _ => {
+                    match memo.get(&n) {
+                        Some(value) => return *value,
+                        None => {
+                            let value = climber(n.clone() - 1, memo) + climber(n.clone() - 2, memo);
+                            memo.insert(n, value);
+                            return value;
+                        }
+                    }
+                }
+            }
+        };
+
+        climber(n, &mut memo)
+    }
+}
