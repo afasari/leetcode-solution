fa6bf68b18582cc296b72baedf53cbdfc7f6ca36
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2024-10-31 10:02:23 +0700

    Time: 2 ms (62.25%), Space: 4 MB (10.41%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.go
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -1,43 +1,50 @@
 func mergeAlternately(word1 string, word2 string) string {
-    l1 := len(word1)
-    l2 := len(word2)
+    // l1 := len(word1)
+    // l2 := len(word2)
     
-    var ret string
+    // var ret string
     
-    if l1 < l2 {
-        for i, w := range word1{
-            ret += string(w) + string(word2[i])
-        }
+    
+//     if l1 < l2 {
+//         for i, w := range word1{
+//             ret += string(w) + string(word2[i])
+//         }
         
-        ret += word2[l1:l2]
-    } else {
-        for i, w := range word1{
-            if i == l2 {
-                ret += word1[i:l1]
-                break
-            }
+//         ret += word2[l1:l2]
+//     } else {
+//         for i, w := range word1{
+//             if i == l2 {
+//                 ret += word1[i:l1]
+//                 break
+//             }
 
-            ret += string(w) + string(word2[i])
-        }
-    }
+//             ret += string(w) + string(word2[i])
+//         }
+//     }
 
-    return ret
+    // return ret
+    
+    return handleMerge(word1, word2)
 }
 
 
-func handleMerge(w1, w2 string, maxSame int) string {
+func handleMerge(w1, w2 string) string {
     var ret string
+    
+    isCheckInside := len(w1) > len(w2)
     for i, w := range w1{
-        if i == maxSame {
-            fmt.Println(ret)
-            fmt.Println(w1[i:len(w1)+1])
-            ret += w1[i:len(w1)+1]
+        if isCheckInside && i == len(w2) {
+            ret += w1[i:len(w1)]
             break
-        } else {
-            fmt.Println("ret", 1, string(w), ret)
         }
         
         ret += string(w) + string(w2[i])
     }
+    
+    if !isCheckInside{
+        ret += w2[len(w1):len(w2)]
+    }
+    
+    
     return ret
 }
