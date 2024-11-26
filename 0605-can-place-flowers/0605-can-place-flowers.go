a7a6c1eb7c0f3dda25efa37e7a69760863f83f2b
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2024-11-26 18:43:12 +0700

    Time: 0 ms (100.00%), Space: 8.1 MB (18.42%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.go
@@ -0,0 +1,16 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    // greedy
+    // O(N)
+    // O(1)
+    m := len(flowerbed)
+    total := 0
+    for i := 0; i < m; i++ {
+        if flowerbed[i] == 0 {
+            if (i == 0 || flowerbed[i - 1] == 0) && (i == m - 1 || flowerbed[i + 1] == 0) {
+                total++
+                flowerbed[i] = 1
+            }
+        }
+    }
+    return total >= n
+}
