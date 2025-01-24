0767de89110506166fe9ab066bb693c001b2bd1b
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-01-24 11:11:09 +0700

    Time: 0 ms (100%), Space: 8.6 MB (75.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.go
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -4,15 +4,28 @@ func moveZeroes(nums []int)  {
     if l == 0{
         return
     }
+
+    twoPointer(nums)
     
-    var count, idx int
-    for count != l{
-        if nums[idx] == 0{
-            nums = append(nums[:idx], nums[idx+1:]...)
-            nums = append(nums, 0)
-        } else {
-            idx++
+    // var count, idx int
+    // for count != l{
+    //     if nums[idx] == 0{
+    //         nums = append(nums[:idx], nums[idx+1:]...)
+    //         nums = append(nums, 0)
+    //     } else {
+    //         idx++
+    //     }
+    //     count++
+    // }
+}
+
+func twoPointer(nums []int){
+    left := 0
+    for right := 0; right < len(nums); right++{
+        if nums[right] != 0{
+            nums[left], nums[right] = nums[right], nums[left]
+            left++
         }
-        count++
+
     }
 }
