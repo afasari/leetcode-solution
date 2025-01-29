ed40a1694bc432f6a57100e98f5f0932e968496a
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-01-29 16:18:00 +0700

    Time: 0 ms (100%), Space: 13 MB (12.61%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.go
@@ -0,0 +1,26 @@
+import (
+    "math"
+)
+
+func findMaxAverage(nums []int, k int) float64 {
+    currentVal := 0
+    maxVal := math.MinInt
+
+    for i, num := range nums{
+        currentVal += num
+
+        if k-1 < i {
+            currentVal -= nums[i-k]
+        } 
+        
+        if k-1 <= i {
+            if currentVal > maxVal{
+                maxVal = currentVal
+            }
+        }
+
+
+    }
+
+    return float64(maxVal)/ float64(k)
+}
