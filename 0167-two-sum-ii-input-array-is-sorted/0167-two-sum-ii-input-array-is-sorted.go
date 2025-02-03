b59681b94c1ab535312a68f69013f2b6d0c73f4d
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-02-03 19:39:59 +0700

    Time: 0 ms (100%), Space: 7.8 MB (71.59%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -1,11 +1,20 @@
 func twoSum(numbers []int, target int) []int {
-    for i:=0; i < len(numbers) -1; i++{
-        for j:= 1; j < len(numbers);j++{
-            if i!= j && numbers[i] + numbers[j] == target{
-                return []int{i+1,j+1}
-            }
+    m := make(map[int]int)
+
+    for i, num := range numbers{
+        if j, ok := m[target-num]; ok{
+            return []int{j+1,i+1}
+        } else{
+            m[num] = i
         }
     }
-    
+    // for i:=0; i < len(numbers) -1; i++{
+    //     for j:= 1; j < len(numbers);j++{
+    //         if i!= j && numbers[i] + numbers[j] == target{
+    //             return []int{i+1,j+1}
+    //         }
+    //     }
+    // }
+
     return []int{}
 }
