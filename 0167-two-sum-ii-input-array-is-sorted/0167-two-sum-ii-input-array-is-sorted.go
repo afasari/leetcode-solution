0ca2d27f1f70136e9854fdd33cc602062fde7b9b
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-02-03 19:46:03 +0700

    Time: 0 ms (100%), Space: 8.1 MB (17.2%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -1,13 +1,35 @@
 func twoSum(numbers []int, target int) []int {
-    m := make(map[int]int)
+    // O(n) two pointer
+    l, r := 0, len(numbers)-1
+    for r > l{
+        count := numbers[l] + numbers[r]
 
-    for i, num := range numbers{
-        if j, ok := m[target-num]; ok{
-            return []int{j+1,i+1}
-        } else{
-            m[num] = i
+        if count > target {
+            r--
+        } else if count < target {
+            l++
+        } else {
+            return []int{l+1, r+1}
         }
     }
+
+    return []int{}
+
+    // O(n) hashmap
+    // m := make(map[int]int)
+
+    // for i, num := range numbers{
+    //     if num > target {
+    //         return []int{}
+    //     }
+    //     if j, ok := m[target-num]; ok{
+    //         return []int{j+1,i+1}
+    //     } else{
+    //         m[num] = i
+    //     }
+    // }
+
+    // O(N2)
     // for i:=0; i < len(numbers) -1; i++{
     //     for j:= 1; j < len(numbers);j++{
     //         if i!= j && numbers[i] + numbers[j] == target{
