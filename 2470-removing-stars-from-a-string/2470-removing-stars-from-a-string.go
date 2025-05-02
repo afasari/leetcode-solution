cdf8c873ce928ffd72e63fec0537c9b8a39c074c
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-05-02 16:25:15 +0700

    Time: 9 ms (60.06%), Space: 9.3 MB (39.16%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.go
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.go
@@ -1,16 +1,30 @@
 func removeStars(s string) string {
-    ret := make([]byte, len(s))
-    idx := 0
-    
+    ret := []byte{}
+
     for i, char := range s {
-        if char != '*' {
-            ret[idx] = s[i]
-            idx++
+        if char == '*' {
+            ret = ret[:len(ret)-1]
         } else {
-            ret[idx] = 0
-            idx--
+            ret = append(ret, s[i])
         }
     }
 
-    return string(ret[:idx])
+    return string(ret)
 }
+
+// func removeStars(s string) string {
+//     ret := make([]byte, len(s))
+//     idx := 0
+    
+//     for i, char := range s {
+//         if char != '*' {
+//             ret[idx] = s[i]
+//             idx++
+//         } else {
+//             ret[idx] = 0
+//             idx--
+//         }
+//     }
+
+//     return string(ret[:idx])
+// }
