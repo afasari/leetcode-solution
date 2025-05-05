003955c542f1f4422a4c72e8466cfd020c8f45e9
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-05-05 09:36:18 +0700

    Time: 0 ms (100%), Space: 6.6 MB (73.29%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.go
+++ 0735-asteroid-collision/0735-asteroid-collision.go
@@ -1,11 +1,11 @@
 func asteroidCollision(astr []int) []int {
     prev := -1
     for i := 0; i < len(astr); i++ {
-        notSame := true
         if astr[i] > 0 {
             prev++
             astr[prev] = astr[i]
         } else {
+            notSame := true
             for prev >= 0 && astr[prev] > 0 {
                 if astr[prev] < -astr[i] {
                     prev--
