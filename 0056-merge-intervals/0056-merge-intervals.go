25335fff635b830dc2f5feb99b2bbefde2d375c3
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-07-24 09:07:11 +0700

    Time: 3 ms (60.05%), Space: 8.7 MB (26.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.go
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -1,17 +1,19 @@
 func merge(intervals [][]int) [][]int {
-    sort.Slice(intervals, func(i,j int) bool {
-        return intervals[i][0] < intervals[j][0]
-    })
-
     if len(intervals) < 2 {
         return intervals
     }
 
+    sort.Slice(intervals, func(i,j int) bool {
+        return intervals[i][0] < intervals[j][0]
+    })
+
+
     res := [][]int{intervals[0]}
     for _, interval := range intervals[1:] {
-        if interval[0] <= res[len(res)-1][1] {
-            if interval[1] >= res[len(res)-1][1] {
-                res[len(res)-1][1] = interval[1]
+        lastMerged := res[len(res)-1]
+        if interval[0] <= lastMerged[1] {
+            if interval[1] >= lastMerged[1] {
+                lastMerged[1] = interval[1]
             }
         } else {
             res = append(res, interval)
