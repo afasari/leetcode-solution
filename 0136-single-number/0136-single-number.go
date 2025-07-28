f9151d644f2e4c0377db2f70b260f7c3086bd417
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-07-28 10:27:37 +0700

    Time: 0 ms (100%), Space: 8.1 MB (53.09%) - LeetHub

--- 0136-single-number/0136-single-number.go
+++ 0136-single-number/0136-single-number.go
@@ -1,26 +1,4 @@
 func singleNumber(nums []int) int {
-//     m := make(map[int]bool)
-//     for _, num := range nums{
-//         if val, ok := m[num]; ok && val{
-//             fmt.Println("masuk del", num)
-//             delete(m, num)
-//         } else {
-//          m[num] = true           
-//         }
-//     }
-    
-//     if len(m) == 0{
-//         return -1
-//     }
-    
-    
-//     fmt.Println(m)
-//     for mv, _ := range m {
-//         return mv
-//     }
-    
-//     return  -1 
-    
     res := 0
     for _, num := range nums{
         res ^= num
