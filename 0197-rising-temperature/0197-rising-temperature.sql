02d541d9a1eaef3db5a711572af6c52185648853
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-08-06 16:31:44 +0700

    Time: 598 ms (13.46%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -6,3 +6,50 @@ WHERE
     DATEDIFF(w2.recordDate, w1.recordDate) = 1 
 AND 
     w2.temperature > w1.temperature;
+
+
+-- SELECT 
+--     w1.id
+-- FROM 
+--     Weather w1
+-- JOIN 
+--     Weather w2
+-- ON 
+--     DATEDIFF(w1.recordDate, w2.recordDate) = 1
+-- WHERE 
+--     w1.temperature > w2.temperature;
+
+
+-- SELECT 
+--     w1.id
+-- FROM 
+--     Weather w1
+-- WHERE 
+--     w1.temperature > (
+--         SELECT 
+--             w2.temperature
+--         FROM 
+--             Weather w2
+--         WHERE 
+--             w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+--     );
+
+-- WITH PreviousWeatherData AS
+-- (
+--     SELECT 
+--         id,
+--         recordDate,
+--         temperature, 
+--         LAG(temperature, 1) OVER (ORDER BY recordDate) AS PreviousTemperature,
+--         LAG(recordDate, 1) OVER (ORDER BY recordDate) AS PreviousRecordDate
+--     FROM 
+--         Weather
+-- )
+-- SELECT 
+--     id 
+-- FROM 
+--     PreviousWeatherData
+-- WHERE 
+--     temperature > PreviousTemperature
+-- AND 
+--     recordDate = DATE_ADD(PreviousRecordDate, INTERVAL 1 DAY);
