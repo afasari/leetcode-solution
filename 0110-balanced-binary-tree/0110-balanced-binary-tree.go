36c61d91da232ba3a05769fffcf1cab861cc28e0
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-11-26 13:22:04 +0700

    Time: 0 ms (100%), Space: 7.3 MB (54.71%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+    
+    diff := maxDepth(root.Left) - maxDepth(root.Right)
+
+    if diff <= 1 &&diff >= -1 {
+        return isBalanced(root.Left) && isBalanced(root.Right)
+    }
+
+    return false
+}
+
+
+func maxDepth(root *TreeNode) int{
+    if root == nil{
+        return 0
+    }
+    
+    return max(maxDepth(root.Left),maxDepth(root.Right))+1
+}
