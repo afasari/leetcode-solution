f27a95a874e58dd9a541e608ff71660210306a3f
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-18 14:57:10 +0700

    Time: 2 ms (56.28%), Space: 4.7 MB (11.88%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.go
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -1,27 +1,4 @@
 func mergeAlternately(word1 string, word2 string) string {
-//     l1 := len(word1)
-//     l2 := len(word2)
-    
-//     var ret string
-    
-//     isCheckInside := l1 > l2
-//     for i, w := range word1{
-//         if isCheckInside && i == l2 {
-//             ret += word1[i:l1]
-//             break
-//         }
-        
-//         ret += string(w) + string(word2[i])
-//     }
-    
-//     if !isCheckInside{
-//         ret += word2[l1:l2]
-//     }
-    
-//     return ret
-    
-    
-    // var res []byte
     var res string
     for i:= 0; i < len(word1) || i < len(word2); i++ {
         if i < len(word1) {
@@ -34,27 +11,4 @@ func mergeAlternately(word1 string, word2 string) string {
     }
     
     return res
-    // return handleMerge(word1, word2)
 }
-
-
-// func handleMerge(w1, w2 string) string {
-//     var ret string
-    
-//     isCheckInside := len(w1) > len(w2)
-//     for i, w := range w1{
-//         if isCheckInside && i == len(w2) {
-//             ret += w1[i:len(w1)]
-//             break
-//         }
-        
-//         ret += string(w) + string(w2[i])
-//     }
-    
-//     if !isCheckInside{
-//         ret += w2[len(w1):len(w2)]
-//     }
-    
-    
-//     return ret
-// }
