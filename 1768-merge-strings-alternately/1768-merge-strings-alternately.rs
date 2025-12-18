4d9e783a452ab9ded9930ac34ab7faaf25cdb1d9
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-18 15:25:35 +0700

    Time: 1 ms (49.24%), Space: 2.2 MB (59.09%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.rs
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.rs
@@ -3,6 +3,7 @@ impl Solution {
         let k = word1.len() + word2.len();
         let mut merged = String::with_capacity(k);
         let (mut word1, mut word2) = (word1.chars(), word2.chars());
+        
         for _ in 0..k {
             if let Some(c) = word1.next() {
                 merged.push(c);
