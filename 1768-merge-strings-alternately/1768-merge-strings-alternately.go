5edbfeabe317d5c4916033c4721466fba78a46bf
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-18 15:13:17 +0700

    Time: 0 ms (100%), Space: 4.7 MB (11.88%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.go
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -1,5 +1,6 @@
 func mergeAlternately(word1 string, word2 string) string {
-    var res string
+    res:= ""
+
     for i:= 0; i < len(word1) || i < len(word2); i++ {
         if i < len(word1) {
             res += string(word1[i])
