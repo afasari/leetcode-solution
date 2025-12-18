3b079e214666ea0be6a706a287ef8e128cf850a0
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-18 15:21:28 +0700

    Time: 1 ms (49.24%), Space: 2.2 MB (59.09%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.rs
@@ -0,0 +1,16 @@
+impl Solution {
+     pub fn merge_alternately(word1: String, word2: String) -> String {
+        let k = word1.len() + word2.len();
+        let mut merged = String::with_capacity(k);
+        let (mut word1, mut word2) = (word1.chars(), word2.chars());
+        for _ in 0..k {
+            if let Some(c) = word1.next() {
+                merged.push(c);
+            }
+            if let Some(c) = word2.next() {
+                merged.push(c);
+            }
+        }
+        merged
+    }
+}
