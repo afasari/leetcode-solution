4e3a2a92681f6d3bec0b84016504197c53dd592b
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-23 09:36:06 +0700

    Time: 22 ms (50.25%), Space: 12.5 MB (35.24%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,11 +1,42 @@
 func containsDuplicate(nums []int) bool {
-    hashMap := make(map[int]bool)
+    /*
+    Brute Force 
+    Time O(n^2) 
+    Space O(1)
+    */
+    // for i := 0; i < len(nums); i++ {
+    //     for j := i + 1; j < len(nums); j++ {
+    //         if nums[i] == nums[j] {
+    //             return true
+    //         }
+    //     }
+    // }
+    // return false
 
-    for _, num := range nums{
-        if _, ok := hashMap[num]; ok{
+    /*
+    Sorting
+    Time O(n log n)
+    Space O(log n) (other sort may different)
+    */
+    // sort.Ints(nums)
+    // for i := 1; i < len(nums); i++ {
+    //     if nums[i] == nums[i-1] {
+    //         return true
+    //     }
+    // }
+    // return false
+
+    /*
+    Hash
+    Time O(n)
+    Space O(n)
+    */
+    seen := make(map[int]bool)
+    for _, num := range nums {
+        if seen[num] {
             return true
         }
-        hashMap[num] = true
+        seen[num] = true
     }
     return false
 }
