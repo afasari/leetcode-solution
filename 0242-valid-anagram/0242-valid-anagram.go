ca5ef218fdc0cd207900f4eecdc6644dd71231e2
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-23 11:27:54 +0700

    Time: 4 ms (53.83%), Space: 4.8 MB (35.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,24 +1,67 @@
 func isAnagram(s string, t string) bool {
-    if len(s) != len(t){
+    /*
+    Sorting
+    Time O(n log n + m log m)
+    Space O(1) (other sort may different)
+    */
+    // if len(s) != len(t) {
+    //     return false
+    // }
+
+    // sRunes, tRunes := []rune(s), []rune(t)
+    // sort.Slice(sRunes, func(i, j int) bool {
+    //     return sRunes[i] < sRunes[j]
+    // })
+
+    // sort.Slice(tRunes, func(i, j int) bool {
+    //     return tRunes[i] < tRunes[j]
+    // })
+
+    // return string(sRunes) == string(tRunes)
+
+    /*
+    Hash Map
+    Time O(n + m)
+    Space O(1)
+    */
+    if len(s) != len(t) {
         return false
     }
 
-    mapWord1 := make(map[byte]int)
-    mapWord2 := make(map[byte]int)
-    length := len(s)
-    for i:=0; i< length; i++{
-        mapWord1[s[i]]++
-        mapWord2[t[i]]++
+    countS, countT := make(map[rune]int), make(map[rune]int)
+    for i, ch := range s {
+        countS[ch]++
+        countT[rune(t[i])]++
     }
 
-    for key, val := range mapWord1 {
-        if val != mapWord2[key]{
-return false
+    for k, v := range countS {
+        if countT[k] != v {
+            return false
         }
     }
 
     return true
 
-    // O(s +t) time complexity   
-    // O(s +t) memory complexity
+    /*
+    Hash Table (array)
+    Time O(n + m)
+    Space O(1)
+    */
+    // if len(s) != len(t) {
+    //     return false
+    // }
+
+    // count := [26]int{}
+    // for i := 0; i < len(s); i++ {
+    //     count[s[i]-'a']++
+    //     count[t[i]-'a']--
+    // }
+
+    // for _, val := range count {
+    //     if val != 0 {
+    //         return false
+    //     }
+    // }
+
+    // return true
 }
