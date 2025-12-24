1e2e42fc0e73001c1c9a2a759ce253bcee10ede0
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-24 22:26:53 +0700

    Time: 0 ms (100%), Space: 3.9 MB (97.73%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.go
@@ -0,0 +1,37 @@
+func lastStoneWeight(stones []int) int {
+    if len(stones) == 0 {
+        return 0
+    }
+    
+    if len(stones) == 1 {
+        return stones[0]
+    }
+
+    if len(stones) == 2{
+    return int(math.Abs(float64(stones[0]-stones[1])))
+    }
+
+   for {
+        if len(stones) == 0 { return 0 }
+        if len(stones) == 1 { return stones[0] }
+        x, y := 0, 0
+        index := 0
+        for i, v := range stones {
+            if v > y {
+                y = v
+                index = i
+            }
+        }
+        stones = append(stones[:index], stones[index + 1:]...)
+        for i, v := range stones {
+            if v > x {
+                x = v
+                index = i
+            }
+        }
+        stones = append(stones[:index], stones[index + 1:]...)
+        if y != x { stones = append(stones, y - x) }
+    }
+
+    return -1
+}
