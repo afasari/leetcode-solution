d46b37ccaaf4640b4130f083c4b99bcca4cb9028
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-24 22:39:28 +0700

    Time: 0 ms (100%), Space: 4.8 MB (3.06%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.go
@@ -0,0 +1,13 @@
+func reverseBits(n int) int {
+    bin := fmt.Sprintf("%032b",n)
+
+    ret := ""
+    for i := len(bin)-1; i >= 0 ; i-- {
+        ret += fmt.Sprintf("%v",string(bin[i]))
+    }
+
+    retInt, _ := strconv.ParseInt(ret, 2, 64)
+    fmt.Println(ret)
+    fmt.Println(retInt)
+    return int(retInt)
+}
