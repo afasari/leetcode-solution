3f518e5f68d75c661e3a46089ac5dba717f5e45a
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2025-12-28 08:23:42 +0700

    Time: 0 ms (100%), Space: 8.1 MB (28.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -1,47 +1,25 @@
 func topKFrequent(nums []int, k int) []int {
-    var ret []int
-
-    m := make(map[int]int)
-    for _, num := range nums{
-        m[num]++
+    // 1. Count frequencies: O(n)
+    counts := make(map[int]int)
+    for _, num := range nums {
+        counts[num]++
     }
 
-    mapMost := make(map[int][]int)
-    var sliceCount []int
-
-    for num, count := range m{
-        mapMost[count] = append(mapMost[count], num)
-        sliceCount = append(sliceCount, count)
-        // sliceCount = append([]int{count}, sliceCount...) 
+    // 2. Create buckets where index = frequency: O(n)
+    // bucket[i] contains all numbers that appear i times
+    buckets := make([][]int, len(nums)+1)
+    for num, freq := range counts {
+        buckets[freq] = append(buckets[freq], num)
     }
-    // sort.Ints(sliceCount)
-    sort.Sort(sort.Reverse(sort.IntSlice(sliceCount)))
 
-fmt.Println(sliceCount)
-fmt.Println(mapMost)
-
-    lastCount := 0
-    for _, count := range sliceCount {
-        fmt.Println(count)
-        if count == lastCount{
-            continue
-        }
-        lastCount = count
-        if vals, ok := mapMost[count]; ok{
-                    // fmt.Println(vals)
-        sort.Ints(vals)
-
-            ret = append(ret, vals...)
-        }
-
-        if len(ret) > k{
-            return ret[:k]
+    // 3. Iterate backwards from highest frequency: O(n)
+    result := make([]int, 0, k)
+    for i := len(buckets) - 1; i >= 0 && len(result) < k; i-- {
+        if len(buckets[i]) > 0 {
+            result = append(result, buckets[i]...)
         }
     }
 
-    if len(ret) < k {
-        return ret
-    }
-    
-    return ret[:k]
+    // Return exactly k elements (in case bucket contained more than needed)
+    return result[:k]
 }
