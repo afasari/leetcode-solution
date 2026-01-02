031cc91a9ff53a8eb5ae2287d2e4a67941cfe561
Ba'tiar Afas Rahmamulia <batiar.rahmamulia@gmail.com>
2026-01-02 08:31:51 +0700

    Time: 2 ms (15.72%), Space: 9.6 MB (58.7%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.go
+++ 0011-container-with-most-water/0011-container-with-most-water.go
@@ -1,28 +1,31 @@
 func maxArea(height []int) int {
-    l, water := 0, 0
-    r := len(height)-1
+    maxArea := 0
+    left := 0
+    right := len(height) - 1
 
-    for l < r {
-        hl := height[l]
-        hr := height[r]
-        minH := hl
-
-        if hl > hr{
-            minH = hr
-        }
-
-        temp := (r-l) * minH
-        if temp > water {
-            water = temp
-        }
-
-        if hl > hr {
-            r--
+    for left < right {
+        // 1. Calculate current area
+        w := right - left
+        h := 0
+        if height[left] < height[right] {
+            h = height[left]
         } else {
-            l++
+            h = height[right]
+        }
+        
+        currentArea := w * h
+        
+        // 2. Update global max
+        maxArea = max(maxArea, currentArea)
+
+        // 3. Move the pointer pointing to the shorter line
+        // This is the greedy step to find a taller bottleneck
+        if height[left] < height[right] {
+            left++
+        } else {
+            right--
         }
     }
 
-    return water
-    
+    return maxArea
 }
